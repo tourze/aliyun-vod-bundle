f43eda043e259d2d22bf072480b86aa8847c76fc
github-ci <you@example.com>
2025-06-10 23:49:27 +0800

    修正过时用法

--- src/Controller/VideoUploadController.php
+++ src/Controller/VideoUploadController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Tourze\AliyunVodBundle\Service\AliyunVodConfigService;
 use Tourze\AliyunVodBundle\Service\VideoUploadService;
 
